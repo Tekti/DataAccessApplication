771cd5c0b05959b2d335b82314dfe142fd432e73
Tekti <radek@paranoid.me>
2020-06-14 12:48:38 +0200

    Add project files.

---
+++ DataAccess/Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataLibrary;
+using DataLibrary.BusinessLogic;
+
+namespace DataAccess.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ViewEmployees()
+        {
+            ViewBag.Message = "Employees List";
+
+            var data = EmployeeProcessor.LoadEmployees();
+
+            List<EmployeeModel> employees = new List<EmployeeModel>();
+
+            foreach (var row in data)
+            {
+                employees.Add(new EmployeeModel
+                {
+                    EmployeeId = row.EmployeeId,
+                    FirstName = row.FirstName,
+                    LastName = row.LastName,
+                    EmailAddress = row.EmailAddress
+                });
+            }
+
+            return View(employees);
+        }
+
+        public ActionResult SignUp()
+        {
+            ViewBag.Message = "Employee ";
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SignUp(EmployeeModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                int recordsCreated = EmployeeProcessor.CreateEmployee(model.EmployeeId,
+                    model.FirstName,
+                    model.LastName,
+                    model.EmailAddress);
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ DataLibrary/BusinessLogic/EmployeeProcessor.cs
@@ -0,0 +1,38 @@
+﻿using DataLibrary.DataAccess;
+using DataLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.BusinessLogic
+{
+    public static class EmployeeProcessor
+    {
+        public static int CreateEmployee(int employeeId, string firstname,
+            string lastName, string emailAddress)
+        {
+            EmployeeModel data = new EmployeeModel
+            {
+                EmployeeId = employeeId,
+                FirstName = firstname,
+                LastName = lastName,
+                EmailAddress = emailAddress
+            };
+
+            string sql = @"INSERT INTO dbo.EmployeeTable (EmployeeId, FirstName, LastName, EmailAddress)
+                            values (@EmployeeId, @FirstName, @LastName, @EmailAddress);";
+
+            return SqlDataAccess.SaveData(sql, data);
+        }
+
+        public static List<EmployeeModel> LoadEmployees()
+        {
+            string sql = @"SELECT Id, EmployeeId, FirstName, LastName, EmailAddress
+                            from dbo.EmployeeTable";
+
+            return SqlDataAccess.LoadData<EmployeeModel>(sql);
+        }
+    }
+}
